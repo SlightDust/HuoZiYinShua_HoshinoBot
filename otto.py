5ab9b0923fcb4492c35ae9a0efc0d075458116e0
SlightDust <zzc_200014@163.com>
2023-12-27 21:46:49 +0800

    hoshino已测试

--- otto.py
+++ otto.py
@@ -1,9 +1,36 @@
 import os
 
-# self_path = os.path.join(os.path.dirname(__file__), 'latest.json')
-self_path = os.path.dirname(__file__)
+from nonebot import MessageSegment
 
-from HuoZiYinShua.huoZiYinShua import huoZiYinShua
-import re
-HZYS = huoZiYinShua("./HuoZiYinShua/settings.json")
-print("ok")
+import hoshino
+from hoshino import Service,priv
+
+from .HuoZiYinShua.huoZiYinShua import huoZiYinShua
+
+self_path = os.path.dirname(__file__)
+settings = {
+    "sourceDirectory": os.path.join(self_path, 'HuoZiYinShua', 'sources',''),
+    "ysddSourceDirectory": os.path.join(self_path, 'HuoZiYinShua', 'ysddSources',''),
+    "dictFile": os.path.join(self_path, 'HuoZiYinShua', 'dictionary.json'),
+    "ysddTableFile": os.path.join(self_path, 'HuoZiYinShua', 'ysddTable.json')
+}
+
+HZYS = huoZiYinShua(settings)
+
+sv = Service(
+    name = 'ottohzys',  #功能名
+    use_priv = priv.NORMAL, #使用权限   
+    manage_priv = priv.ADMIN, #管理权限
+    visible = True, #False隐藏
+    enable_on_default = True, #是否默认启用
+    )
+
+# self_path = os.path.dirname(__file__)
+
+@sv.on_prefix("otto",only_to_me=False)
+async def ottohzys(bot, ev):
+    txt = ev.message.extract_plain_text() + "，，，" # 加一段空白
+    b64audio = HZYS.export(txt,inYsddMode=True)
+    await bot.send(ev, MessageSegment.record(f"base64://{b64audio.decode()}"))
+
+
